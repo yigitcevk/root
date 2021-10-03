186bcfa389567d5b92b917792d3610039cf636a0
yigitcevk <yigit.cevikk@gmail.com>
2021-10-03 20:37:49 +0300

    update

--- app/src/main/java/root/App.java
+++ app/src/main/java/root/App.java
@@ -3,9 +3,14 @@
  */
 package root;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
 import java.util.ArrayList;
-
-//import static spark.Spark.get;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
 
 public class App {
     public String getGreeting() {
@@ -13,16 +18,36 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-        /*
+        port(getHerokuAssignedPort());
+        get("/", (req, res) -> "Hello, World");
+        post("/compute", (req, res) -> {
+        //System.out.println(req.queryParams("input1"));
+        //System.out.println(req.queryParams("input2"));
+        String input1 = req.queryParams("input1");
+        java.util.Scanner sc1 = new java.util.Scanner(input1);
+        sc1.useDelimiter("[;\r\n]+");
+        java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+        while (sc1.hasNext())
+        {
+        int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+        inputList.add(value);
+        }
+        sc1.close();
+        System.out.println(inputList);
+        String input2 = req.queryParams("input2").replaceAll("\\s","");
+        int input2AsInt = Integer.parseInt(input2);
+        boolean result = App.search(inputList, input2AsInt);
+        Map<String, Boolean> map = new HashMap<String, Boolean>();
+        map.put("result", result);
+        return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
         get("/compute",
-            (rq,rs) -> {
-                Map<String, String> map = new HashMap<String, String>();
-                map.put("result","not computed yet!");
-                return new ModelAndView(map, "compute.mustache");
-                },
-                new MustacheTemplateEngine()
-        );*/
+        (rq, rs) -> {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("result", "not computed yet!");
+        return new ModelAndView(map, "compute.mustache");
+    },
+    new MustacheTemplateEngine());
 
     }
 
@@ -36,5 +61,12 @@ public class App {
         }
         return false;
     }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+        return Integer.parseInt(processBuilder.environment().get("PORT"));
+        } return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+        }
 
 }
